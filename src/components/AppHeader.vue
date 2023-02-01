7780835a7ed0daaabd1673661f4c441594aebece
David <djb950@gmail.com>
2023-01-31 19:42:43 -0500

    initial commit. Created a view to display tasks

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <HeaderButton text="Add Task" color="green" />
+    </header>
+</template>
+
+<script>
+import HeaderButton from './HeaderButton.vue'
+
+export default {
+    name: 'AppHeader',
+    props: {
+        title: {
+            type: String,
+            default: 'Hello World'
+        }
+    },
+    components: {
+        HeaderButton
+    }
+}
+
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}</style>
---
+++ src/components/AppTasks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+    <AppTask :task="task" />
+    </div>
+</template>
+
+<script>
+import AppTask from './AppTask.vue'
+
+    export default {
+        name: 'AppTasks',
+        props: {
+            tasks: Array
+        },
+        components: {
+            AppTask
+        }
+    }
+</script>
